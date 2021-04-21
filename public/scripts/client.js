626a5d20d2b0c4bb0b23bc8770ebbb656e09a249
Tyler Bratt <tyler~bratt@hotmail.com>
2021-04-21 16:40:32 -0400

    added ajax post capability to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,50 +3,61 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+//pass the tweet date instead of 'new DATE'
+//Creates the time element of the tweet
+
 $(document).ready(function () {
-  //pass the tweet date instead of 'new DATE'
-  //Creates the time element of the tweet
-  $(".timeSince").text(timeago.format(new Date()));
-
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense, donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
   //Loops through tweets, calls createTweetElement for each tweet, takes return value and appends it to the tweets container.
   const renderTweets = function (tweets) {
     // console.log(tweets);
     for (const tweet of tweets) {
-      $(".tweetContainer").prepend(createTweetElement(tweet));
-      console.log(tweets, tweet);
+      $(".singleTweet").prepend(createTweetElement(tweet));
     }
   };
 
   //These are the properties that will be rendered to the page
   const createTweetElement = function (tweet) {
-    const { name, avatar, handle } = tweet.user;
-    const { text } = tweet.content.text;
-    const { created_at } = tweet;
+    const htmlElem = `
+    <article class='tweetContainer'>
+      <header>
+        <div>
+          <img src="${tweet.user.avatars}">
+          <span class="name">${tweet.user.name}</span>
+        </div>
+        <span class="handle">${tweet.user.handle}</span>
+      </header>
+      <p class="tweeted">${tweet.content.text}</p>
+      <footer>
+        <span class="timeSince">${tweet.created_at}</span>
+        <div>
+          <span class="flag"><i class="fas fa-flag"></i></span>
+          <span class="retweet"><i class="fas fa-retweet"></i></span>
+          <span class="heart"><i class="fas fa-heart"></i></span>
+        </div>
+      </footer>
+    </article>
+    `;
+
+    return htmlElem;
   };
-  renderTweets(data);
+
+  const loadTweets = () => {
+    $.ajax({
+      url: "/tweets",
+      dataType: "JSON",
+      type: "GET",
+      success: (database) => {
+        renderTweets(database);
+      },
+    });
+  };
+
+  $("#textForm").on("submit", function (evt) {
+    evt.preventDefault();
+  });
+  $.post("/new", $("#tweetForm").serialize());
+  //aks about the correct route
+
+  loadTweets();
+  $(".timeSince").text(timeago.format(new Date()));
 });
